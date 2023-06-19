dcc22dd22d53bdc1f502c8478f810721cd19c818
febri <fitrafilthmongers@gmail.com>
2023-06-19 21:03:02 +0700

    tugas 2 js
    mengumpulkan tugas 2 js

---
+++ tugas2.js
@@ -0,0 +1,14 @@
+var number = 5;
+var string = "abcdefgh";
+var boolean = true;
+var nul = null;
+var undefined;
+var array =[1,2,3,4,5]
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(nul)
+console.log(undefined)
+console.log(array)
+
